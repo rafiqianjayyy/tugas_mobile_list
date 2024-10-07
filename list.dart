b77280a1b4f1c2314f4e4afd8566f7c25a0321d8
rafiqi <muhammadrafiqi171@gmail.com>
2024-10-07 11:53:32 +0700

    Create list.dart

---
+++ list.dart
@@ -0,0 +1,54 @@
+//INI ADALAH CONTOH MEMBUAT LIST DI BAHASA DART
+// void main(){
+//   List<int> listinteger = [];
+//   var listString = <String>[];
+// }
+
+//INI ADALAH CARA MEMASUKAN ATAU MENAMBAHKAN VALUE DI DALAM LIST DART
+// void main(){
+//   var nama = <String>[];
+
+//   nama.add('Muhammad');
+//   nama.add('Rafiqi');
+//   nama.add('.');
+
+//   print(nama);
+//   print(nama.length);
+// }
+
+//INI ADALAH CONTOH MEMANIPULASI DATA DI DALAM LIST
+// void main(){
+//   var nama = <String>[];
+
+//   nama.add('Muhammad');
+//   nama.add('Rafiqi');
+//   nama.add('.');
+
+//   print(nama);
+//   print(nama.length);
+
+//   print(nama[0]);
+//   nama[0] = 'Aku';
+//   nama.removeAt(2);
+//   print(nama);
+// }
+
+//CARA MEMBUAT LIST DENGAN DEKLARASI SECARA LANGSUNG
+// void main(){
+//   var namaOrang = ['iki', 'iky', 'iqi'];
+//   var umurOrang = <int>[18, 28, 19];
+//   var nilaiOrang = [
+//     90,
+//     80,
+//     87
+//   ];
+
+//   print(namaOrang);
+//   print(umurOrang);
+//   print(nilaiOrang);
+// }
+
+//   print(namaOrang);
+//   print(umurOrang);
+//   print(nilaiOrang);
+// }
